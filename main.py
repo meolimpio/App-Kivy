c8185bb3f93c90ef5b9ec1c4b18c327cf540ab2c
Eduarda Olímpio <93235055+MEduardaOl@users.noreply.github.com>
2023-07-07 15:50:12 -0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class SayHello(App):
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+        
+        self.inside.add_widget(Label(text = "Nome: "))
+        self.name = TextInput(multiline = False)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text = "Idade: "))
+        self.idade = TextInput(multiline = False)
+        self.inside.add_widget(self.idade)
+
+        self.inside.add_widget(Label(text = "Email: "))
+        self.email = TextInput(multiline = False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text = "Enviar", font_size = 20)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        nome = self.name.text
+        idade = self.idade.text
+        email = self.email.text
+
+        print("Nome: ", nome, "Idade: ", idade, "Email: ", email)
+
+class MyApp(App):
     def build(self):
-        return Label(text = "Say Hello")
+        return MyGrid()
 
 if __name__ == "__main__":
-    SayHello().run()
+    MyApp().run()
